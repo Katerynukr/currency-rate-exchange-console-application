d7ab663c363a31fdb2622e646650a22d0a4ab806
katerynukr <kateryna_neholiuk@ukr.net>
2021-04-06 14:18:23 +0300

    loading json file

---
+++ CurrencyExchangeRateTest/FetchDataTests.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyExchangeRate.Services;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CurrencyExchangeRateTest
+{
+    public class FetchDataTests
+    {
+        [Fact]
+        public void LoadJson_GivePathToJSONFile()
+        {
+            // Arange
+            var path = "C:\\Users\\kater\\Documents\\dot-net\\CurrencyExchangeRate\\CurrencyExchangeRate\\Data\\ExchangeRates.json";
+            FetchData fetchData = new FetchData();
+            //Act
+            var result = fetchData.LoadJson(path);
+            //Assert
+            result.Should().NotBeEmpty();
+        }
+    }
+}
